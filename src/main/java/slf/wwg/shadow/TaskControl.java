8aec4f2b4bb8982d3c6ba927760212763d1263aa
wwg-pc <361788095@qq.com>
2021-09-25 00:48:02 +0800

    add code

--- src/main/java/slf/wwg/shadow/TaskControl.java
+++ src/main/java/slf/wwg/shadow/TaskControl.java
@@ -1,5 +1,27 @@
 package slf.wwg.shadow;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class TaskControl {
+
+    private final ThreadPoolExecutor boss;
+
+    private final ThreadPoolExecutor worker;
+
+    public TaskControl(int bossSize, int bossQueueSize, int workerSize, int workerQueueSize) {
+        boss = new ThreadPoolExecutor(bossSize, bossSize,
+                30, TimeUnit.SECONDS, new LinkedBlockingQueue<>(bossQueueSize));
+
+        worker = new ThreadPoolExecutor(workerSize, workerSize,
+                30, TimeUnit.SECONDS, new ArrayBlockingQueue<>(workerQueueSize));
+
+    }
+
+    public void submitTask() {
+
+    }
 
 }
